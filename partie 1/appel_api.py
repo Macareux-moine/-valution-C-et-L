ae19a5eb3bd31f5e191061623d80bba57e71e0d7
Macareux-moine <166114484+Macareux-moine@users.noreply.github.com>
2024-04-22 11:31:31 +0200

    Update appel_api.py

--- partie 1/appel_api.py
+++ partie 1/appel_api.py
@@ -1,5 +1,14 @@
 import requests
 
+def Sélection():
+    choice = choice_var.get()
+    if choice == "Ville":
+        Ville()
+    elif choice == "Département":
+        departement()
+    elif choice == "Code postal":
+        code_postal()
+
 def appel_api(query):
     if query.isdigit():  # Si l'entrée est un numéro, il s'agit d'un département
         url = f"https://geo.api.gouv.fr/departements/{query}/communes"
@@ -17,9 +26,15 @@ def appel_api(query):
     except requests.exceptions.RequestException as err:
         print(f"Erreur lors de la requête à l'API : {err}")
         return None
-
-def comunes(commune):  
-    return commune.get("population", 0)
+    
+def comunes(communes_list):
+    total_population = 0
+    if isinstance(communes_list, list):
+        for commune in communes_list:
+            total_population += commune.get("population", 0)
+    else:
+        total_population = communes_list.get("population", 0)
+    return total_population
 
 def departement():
     url = "https://geo.api.gouv.fr/departements"
@@ -34,3 +49,19 @@ def departement():
     except requests.exceptions.RequestException as err:
         print(f"Erreur lors de la requête à l'API : {err}")
         return None
+
+def code_postal(query):
+    url = f"https://geo.api.gouv.fr/communes?codePostal={query}&fields=population"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        population = sum(comunes(commune) for commune in data)
+        return population
+    except requests.exceptions.HTTPError as err:
+        print(f"Erreur lors de la requête à l'API : {err}")
+        return None
+    except requests.exceptions.RequestException as err:
+        print(f"Erreur lors de la requête à l'API : {err}")
+        return None
+
